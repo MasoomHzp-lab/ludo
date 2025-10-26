fec0010894f67fec2a19f50fa83b5b4d62c8af53
habiballahi-zahr <habiballahizahra11@gamil.com>
2025-10-26 10:30:43 +0330

    merge ButtonScript and MainMenuButtons

--- Assets/Scripts/MainMenu/ButtonsScripts.cs
+++ Assets/Scripts/MainMenu/ButtonsScripts.cs
@@ -3,8 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class ButtonsScripts : MonoBehaviour
 {
-     public void ChangeScene(string sceneName)
+     public GameObject Panel;
+
+    public void OpenPannel(GameObject panel)
+    {
+
+        panel.SetActive(true);
+
+    }
+
+    public void ClosePanel()
+    {
+
+        Panel.SetActive(false);
+
+    }
+
+    
+    public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+     public void PlaySoundButton()
+    {
+
+        AudioManager.Instance.PlayButtonSound();
     }
 }
